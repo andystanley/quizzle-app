a8247485c3d0e7464add9e4a0554b18cdecd1153
Andrew Stanley <andystanley@hotmail.ca>
2018-01-06 16:06:47 -0500

    Made changes to the QuizCreator Activity

--- app/src/main/java/ca/andrewstanley/quizzle/QuizCreatorActivity.java
+++ app/src/main/java/ca/andrewstanley/quizzle/QuizCreatorActivity.java
@@ -3,20 +3,26 @@ package ca.andrewstanley.quizzle;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class QuizCreatorActivity extends Activity {
     QuizDb dbId;
     EditText quizQuestion, answerOne, answerTwo, answerThree,answerFour, correctAnswer, addSubject;
     Button btnSaveQuestion,doneButton;
+    RadioButton existingCat, newCat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_creator);
+
+        //initialize all field variables
         dbId = new QuizDb(this);
-        quizQuestion = (EditText) findViewById(R.id.txt_question);
+        quizQuestion = (EditText) findViewById(R.id.quiz_question);
         answerOne = (EditText) findViewById(R.id.answer_one);
         answerTwo = (EditText) findViewById(R.id.answer_two);
         answerThree = (EditText) findViewById(R.id.answer_three);
@@ -25,30 +31,67 @@ public class QuizCreatorActivity extends Activity {
         addSubject = (EditText) findViewById(R.id.add_subject);
         btnSaveQuestion = (Button) findViewById(R.id.save_question);
         doneButton = (Button) findViewById(R.id.done_button);
+        final Spinner addCat = findViewById(R.id.newCatSpinner);
+        String[] categories = getResources().getStringArray(R.array.categories);
+        existingCat = findViewById(R.id.existingCat);
+        newCat = findViewById(R.id.newCat);
 
-        saveQuestion();
+        existingCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (existingCat.isChecked()){
+                    addSubject.setEnabled(false);
+                    addCat.setEnabled(true);
+                }
+            }
+        });
 
-    }
+        newCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (newCat.isChecked()) {
+                    addCat.setEnabled(false);
+                    addSubject.setEnabled(true);
+                }
+            }
+        });
 
-    public void saveQuestion(){
         btnSaveQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              boolean quizAdded =  dbId.addQuiz(
-                        quizQuestion.getText().toString(),
-                        answerOne.getText().toString(),
-                        answerTwo.getText().toString(),
-                        answerThree.getText().toString(),
-                        answerFour.getText().toString(),
-                        correctAnswer.getText().toString(),
-                        addSubject.getText().toString());
-
-              if (quizAdded == true){
-                  Toast.makeText(QuizCreatorActivity.this,"Questions is Saved",Toast.LENGTH_SHORT).show();
-              }
-              else
-                  Toast.makeText(QuizCreatorActivity.this,"Questions is Not Saved",Toast.LENGTH_SHORT).show();
+                saveQuestion();
             }
         });
+
+        //create instance of array adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+
+                android.R.layout.simple_spinner_item,
+                //pass categories array
+                categories
+        );
+
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        addCat.setAdapter(adapter);
+    }
+
+    public void saveQuestion() {
+        boolean quizAdded = dbId.addQuiz(
+                quizQuestion.getText().toString(),
+                answerOne.getText().toString(),
+                answerTwo.getText().toString(),
+                answerThree.getText().toString(),
+                answerFour.getText().toString(),
+                correctAnswer.getText().toString(),
+                addSubject.getText().toString());
+
+        if (quizAdded == true) {
+            Toast.makeText(QuizCreatorActivity.this, "Question is Saved", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(QuizCreatorActivity.this, "Question is Not Saved", Toast.LENGTH_SHORT).show();
     }
 }
